7c1c7fac459998bb3f7886d36eac2be87407935f
marimontefusco <mari.montefusco@gmail.com>
2024-08-30 20:27:06 -0300

    initial backend codes

---
+++ src/main/java/com/application/vencidinhos/VencidinhosApplication.java
@@ -0,0 +1,15 @@
+package com.application.vencidinhos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VencidinhosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VencidinhosApplication.class, args);
+	}
+
+}
+
+//spring.datasource.url=jdbc:mysql://localhost:3306/bd_vencidinhos?createDatabaseIfNotExist=true&serverTimezone=UTC&useSSl=false
---
+++ src/test/java/com/application/vencidinhos/VencidinhosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.vencidinhos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VencidinhosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
